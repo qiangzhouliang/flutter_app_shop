61a1dd87460241d34d9311c8ce08aafc89dbf609
18309315291 <2538096489@qq.com>
2019-06-24 14:23:48 +0800

    添加了视频播放功能

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_shop/config/service_url.dart';
 import '../service/service_method.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'dart:convert';
